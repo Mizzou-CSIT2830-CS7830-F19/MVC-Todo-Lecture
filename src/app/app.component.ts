e28dab26bfd96e0550a6e97f1d6cce40ce17ec9e
Professor Wergeles <wergelesn@missouri.edu>
2019-10-08 13:52:17 -0500

    MVC-Todo-BeforeClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'todo-app';
+  title = "todo-app";
+
+  todoArray = [];
+
+  addTodo(value) {
+    if (value !== "") {
+      this.todoArray.push(value);
+      console.log(this.todoArray);
+    } else {
+      alert("Field required!");
+    }
+  }
+
+  deleteItem(todo) {
+    for (let i = 0; i < this.todoArray.length; i++) {
+      if (todo == this.todoArray[i]) {
+        this.todoArray.splice(i, 1);
+        console.log("delete item");
+      }
+    }
+  }
+
+  todoSubmit(value: any) {
+    if (value !== "") {
+      this.todoArray.push(value.todo);
+    } else {
+      alert("Field Required");
+    }
+  }
 }
